1a9e2da032c9c53b3e832760ba91ff6c36a9ab8e
krishnakanth Dasi <krishnakanthdasi@gmail.com>
2021-03-12 12:25:32 +0530

    Auto refresh pending

--- InlineGridEditing/Controllers/HomeController.cs
+++ InlineGridEditing/Controllers/HomeController.cs
@@ -14,30 +14,11 @@ namespace InlineGridEditing.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        public static List<EmployeeInline> EmpList;
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            EmpList = new List<EmployeeInline>();
-            EmployeeInline employeesInline;
-
-
-            for (int i = 1; i <= 6; i++)
-            {
-                employeesInline = new EmployeeInline()
-                {
-                    id = i,
-                    firstName = "Name" + i.ToString(),
-                    lastName = "Last name" + i.ToString(),
-                    lastSSN = "Last  SSN" + i.ToString(),
-                    department = "Finance",
-                    age = 30,
-                    salary = "30000",
-                    maritalStatus = "Married",
-                    permenant = "True"
-                };
-                EmpList.Add(employeesInline);
-            }
+             
         }
 
         public IActionResult Index()
@@ -49,23 +30,17 @@ namespace InlineGridEditing.Controllers
         {
             return View();
         }
-        [HttpGetAttribute]
-        public List<EmployeeInline> GetEmployees()
-        {
-              //var jsonSerializer = new JavaScriptSerializer();
-                
-              //  context.Response.Write(jsonSerializer.Serialize(collectionEmployee.AsQueryable<EmployeeInline>().ToList<EmployeeInline>()));
-           return EmpList;
-        }
-
         [HttpPost]
-        public List<EmployeeInline> GetEmployees(EmployeeInline employee)
+        public IActionResult GetEmployees()
         {
+
             //var jsonSerializer = new JavaScriptSerializer();
 
             //  context.Response.Write(jsonSerializer.Serialize(collectionEmployee.AsQueryable<EmployeeInline>().ToList<EmployeeInline>()));
-            return EmpList;
+            return new JsonResult(Data.empList);
         }
+
+        
         [HttpPost]
         public IActionResult EditEmployee(EmployeeInline employee)
         {
@@ -74,28 +49,25 @@ namespace InlineGridEditing.Controllers
             try
             {
 
-            
-            if (employee.id == -1)
-            {
-                employee.id = (EmpList.Count + 1);
-                EmpList.Add(employee);
-            }
-            else
-            {
-
-                foreach (EmployeeInline emp in EmpList.Where(w => w.id == employee.id).ToList())
+                EmployeeInline employeeInline = Data.empList.FirstOrDefault(c => c.id == employee.id);
+                if (employeeInline == null)
                 {
-                    emp.firstName = employee.firstName;
-                    emp.age = employee.age;
-                    emp.department = employee.department;
-                    emp.lastName = employee.lastName;
-                    emp.maritalStatus = employee.maritalStatus;
-                    emp.salary = employee.salary;
-                    emp.lastSSN = employee.lastSSN;
-                    emp.permenant = employee.permenant;
-
+                    employee.id = Data.empList.Count + 1;
+                    Data.empList.Add(employee);
                 }
-            }
+                else
+                {
+                    employeeInline.firstName = employee.firstName;
+                    employeeInline.age = employee.age;
+                    employeeInline.department = employee.department;
+                    employeeInline.lastName = employee.lastName;
+                    employeeInline.maritalStatus = employee.maritalStatus;
+                    employeeInline.salary = employee.salary;
+                    employeeInline.lastSSN = employee.lastSSN;
+                    employeeInline.permenant = employee.permenant;
+                }
+
+               
                 status = true;
             }
             catch (Exception)
@@ -106,23 +78,18 @@ namespace InlineGridEditing.Controllers
             return Json(new { Status = status, EmpId = employee.id });
 
         }
+        
         [HttpPost]
-        public List<EmployeeInline> AddEmployee([FromBody] EmployeeInline employee)
-        {
-            EmpList.Add(employee);
-            return EmpList;
-        }
-        [HttpDelete]
         public IActionResult DeleteEmployee(int id)
         {
             bool status = false;
 
             try
             {
-                EmployeeInline employeeInline = EmpList.FirstOrDefault(c => c.id == id);
+                EmployeeInline employeeInline = Data.empList.FirstOrDefault(c => c.id == id);
                 if (employeeInline != null)
                 {
-                    EmpList.Remove(employeeInline);
+                    Data.empList.Remove(employeeInline);
                     status = true;
                 }
             }
---
+++ InlineGridEditing/Models/Data.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InlineGridEditing.Models
+{
+    public static class Data
+    {
+        public static ICollection<EmployeeInline> empList { get; private set; }
+
+        static Data()
+        {
+
+            List<EmployeeInline> EmpList = new List<EmployeeInline>();
+            EmployeeInline employeesInline;
+
+
+            for (int i = 1; i <= 6; i++)
+            {
+                employeesInline = new EmployeeInline()
+                {
+                    id = i,
+                    firstName = "Name" + i.ToString(),
+                    lastName = "Last name" + i.ToString(),
+                    lastSSN = "Last  SSN" + i.ToString(),
+                    department = "Finance",
+                    age = 30,
+                    salary = "30000",
+                    maritalStatus = "Married",
+                    permenant = "True"
+                };
+                EmpList.Add(employeesInline);
+            }
+
+            empList = EmpList;
+
+        }
+
+    }
+}
